457d670d228ff2681e35f2eac6c9d166c22d4003
lukaforsstrom <luka.forsstrom@koudata.fi>
2026-04-20 12:35:36 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -85,7 +85,7 @@ const connectWebSocket = () => {
         li.innerHTML = `<img class="mod-badge" src="mod-badge.png" alt="mod"><span class="mod-name">${msg.name}</span>: ${msg.text}`;
       } else if (msg.isVip) {
         li.classList.add("vip-message");
-        li.innerHTML = `<span class="vip-badge">⭐</span><span class="vip-name">${msg.name}</span>: ${msg.text}`;
+        li.innerHTML = `<img class="vip-badge" src="vip-badge.png" alt="vip"><span class="vip-name">${msg.name}</span>: ${msg.text}`;
       } else {
         li.textContent = msg.name + ": " + msg.text;
       }
